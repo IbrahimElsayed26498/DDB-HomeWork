842bd6448bf74f64c0efd47e14d9adff6f5ab3a9
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2021-05-26 10:03:01 +0200

    some commit

---
+++ OOP Assignment/src/DeckOfCards.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class DeckOfCards {
+    private Card deckOfCards[];
+    private static int NUMBER_OF_CARDS = 52;
+
+    public DeckOfCards(Card[] deckOfCards) {
+        this.deckOfCards = deckOfCards;
+    }
+    public DeckOfCards(){
+        deckOfCards = new Card[NUMBER_OF_CARDS];
+    }
+    public void fillDeckOfCards(){
+        int i=0;
+        for (int rank=Card.getMinRank(); rank<= Card.getMaxRank(); rank++){
+            for (int suit=0; suit<4; suit++){
+                deckOfCards[i++] = new Card(suit, rank);
+            }
+        }
+    }
+    public void displayDeckOfCards(){
+        System.out.println(this.toString());
+    }
+    public void displayCard(int i){
+        System.out.println(deckOfCards[i].toString());
+    }
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i=0; i<NUMBER_OF_CARDS; i++){
+            s += deckOfCards[i].toString() + "\n";
+        }
+        return s;
+    }
+}
---
+++ OOP Assignment/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DeckOfCards cards = new DeckOfCards();
+        cards.fillDeckOfCards();
+
+        cards.displayDeckOfCards();
+    }
+}
